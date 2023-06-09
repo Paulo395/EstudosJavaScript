c0a5594274e89786b921b0669bbfe650f4daf342
Paulo Oliveira <paulinho395@gmail.com>
2023-06-09 17:40:49 -0300

    Parse

---
+++ ParseEConcatenacao/Js/index.js
@@ -0,0 +1,11 @@
+let num1 = prompt("Digite um número:");
+let num2 = prompt("Digite um número:");
+
+// const num2 = Number("102");
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
+const result = num1 + num2
+
+alert("O Resultado é: " + result);
